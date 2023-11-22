29a33066a01f4c55b350e0c904666bed37935c6e
himanshu094 <himanshushakya940@gmail.com>
2023-11-22 23:14:54 +0530

    fakestoreapi.com =>one way binding Ex
    https://fakestoreapi.com/products/categories
    https://fakestoreapi.com/products
    shopping view with fakesstoreapi.com
    it is example of one way binding

--- react-shopping/src/index.js
+++ react-shopping/src/index.js
@@ -4,20 +4,18 @@ import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import App from './App';
-import  {NetflixRegisterComponent}  from './component/NetflixRegisterComponent';
-import NetflixIndexComponent from './component/NetflixIndexComponent';
-import LoginComponent from './component/LoginComponent';
-
 import reportWebVitals from './reportWebVitals';
-import ApiComponent from './component/ApiComponent';
+
+import ShoppingComponent from './component/ShoppingComponent';
 import ApiComponentCart from './component/ApiComponentCart';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <NetflixIndexComponent /> */}
-    {/* <LoginComponent/> */}
-    <ApiComponentCart/>
+    {/* <ApiComponentCart/> */}
+    <ShoppingComponent/>
   </React.StrictMode>
 );
 
